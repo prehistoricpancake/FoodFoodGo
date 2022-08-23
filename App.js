9747c3a0086714ddf0374b6e917e9fb50f0002da
Joyce Wambui <jwambui@protonmail.com>
2022-08-23 12:46:28 +0300

    Optimize icon code

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import { ThemeProvider } from 'styled-components/native';
 import { theme } from './src/infra/theme/index';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from "@expo/vector-icons";
 import {
   useFonts as useOswald,
   Oswald_400Regular,
@@ -18,6 +19,12 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 
 const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Restaurants: 'md-restaurant',
+  Map: 'md-map',
+  Settings: 'md-settings'
+}
 
 const Settings = () => (
   <SafeArea>
@@ -31,6 +38,17 @@ const Map = () => (
   </SafeArea>
 )
 
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+
+  return {
+    tabBarIcon: ({
+      size, color
+    }) => (
+      <Ionicons name={iconName}  size={size} color={color} />
+    )
+  }
+}
 
 
 export default function App() {
@@ -51,7 +69,13 @@ export default function App() {
       <>
       <ThemeProvider theme={theme}>
       <NavigationContainer>
-        <Tab.Navigator>
+          <Tab.Navigator
+          screenOptions={createScreenOptions}
+          tabBarOptions={{
+            activeTintColor: "tomato",
+            inactiveTintColor: "gray",
+          }}
+        >
           <Tab.Screen name="Restaurants" component={RestaurantScreen} />
           <Tab.Screen name="Map" component={Map} />
           <Tab.Screen name= "Settings" component={Settings}/>
