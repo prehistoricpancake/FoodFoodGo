f6f3188306c6db2f46e9c96a0452f71f9903de48
Joyce Wambui <jwambui@protonmail.com>
2022-08-30 12:24:08 +0300

    add locations

--- src/features/screens/restaurants.screen.js
+++ src/features/screens/restaurants.screen.js
@@ -3,7 +3,7 @@ import { Searchbar } from "react-native-paper";
 import { FlatList } from "react-native";
 import styled from "styled-components/native";
 import { ActivityIndicator, Colors } from "react-native-paper";
-
+import { Search } from "../../components/search.component";
 import { SafeArea } from "../../components/utility/safe-area.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { Spacer } from "../../components/spacer/spacer.component";
@@ -38,9 +38,7 @@ export const RestaurantsScreen = () => {
           <Loading size={50} animating={true} color={Colors.blue300} />
         </LoadingContainer>
       )}
-      <SearchContainer>
-        <Searchbar />
-      </SearchContainer>
+        <Search />
       <RestaurantList
         data={restaurants}
         renderItem={({ item }) => {
