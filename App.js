976d24721ac80013ee4fd022526edc6a3927b4bf
Joyce Wambui <jwambui@protonmail.com>
2022-08-30 08:56:22 +0300

    couldn't find issue causing 'cellRenderer' error, backtracked and redid the app a bit

--- App.js
+++ App.js
@@ -1,92 +1,83 @@
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, StatusBar, SafeAreaView, Text, View, Platform } from 'react-native';
-import { Searchbar } from 'react-native-paper';
-import RestaurantScreen  from './src/features/restaurants/screens/restaurants.screen';
-import { SafeArea } from "./src/utils/safe-area.component";
-import RestaurantInfo  from './src/features/restaurants/components/restaurants-info-card.components';
-import { ThemeProvider } from 'styled-components/native';
-import { theme } from './src/infra/theme/index';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { Text } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
+
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
-
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
+import { theme } from "./src/infrastructure/theme";
+import { RestaurantsScreen } from "./src/features/screens/restaurants.screen";
+import { SafeArea } from "./src/components/utility/safe-area.component";
+import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 
 const Tab = createBottomTabNavigator();
 
 const TAB_ICON = {
-  Restaurants: 'md-restaurant',
-  Map: 'md-map',
-  Settings: 'md-settings'
-}
+  Restaurants: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+};
 
 const Settings = () => (
   <SafeArea>
     <Text>Settings</Text>
   </SafeArea>
-)
-
+);
 const Map = () => (
   <SafeArea>
     <Text>Map</Text>
   </SafeArea>
-)
+);
 
 const createScreenOptions = ({ route }) => {
   const iconName = TAB_ICON[route.name];
-
   return {
-    tabBarIcon: ({
-      size, color
-    }) => (
-      <Ionicons name={iconName}  size={size} color={color} />
-    )
-  }
-}
-
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
+};
 
 export default function App() {
-  const [ oswaldLoaded ] = useOswald({
-    Oswald_400Regular
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
   });
 
   const [latoLoaded] = useLato({
     Lato_400Regular,
   });
 
-  if(!oswaldLoaded || !latoLoaded){
+  if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
 
   return (
-  
-      <>
+    <>
       <ThemeProvider theme={theme}>
-      <NavigationContainer>
-          <Tab.Navigator
-          screenOptions={createScreenOptions}
-          tabBarOptions={{
-            activeTintColor: "tomato",
-            inactiveTintColor: "gray",
-          }}
-        >
-          <Tab.Screen name="Restaurants" component={RestaurantScreen} />
-          <Tab.Screen name="Map" component={Map} />
-          <Tab.Screen name= "Settings" component={Settings}/>
-        </Tab.Navigator>
-      </NavigationContainer>
+        <RestaurantsContextProvider>
+          <NavigationContainer>
+            <Tab.Navigator
+              screenOptions={createScreenOptions}
+              tabBarOptions={{
+                activeTintColor: "tomato",
+                inactiveTintColor: "gray",
+              }}
+            >
+              <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+              <Tab.Screen name="Map" component={Map} />
+              <Tab.Screen name="Settings" component={Settings} />
+            </Tab.Navigator>
+          </NavigationContainer>
+        </RestaurantsContextProvider>
       </ThemeProvider>
-
-      </>
-   
-
-    );
+      <ExpoStatusBar style="auto" />
+    </>
+  );
 }
-
-const styles = StyleSheet.create({});
