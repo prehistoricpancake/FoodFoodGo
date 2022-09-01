64e2d5db005f0551dc1971dd9a1ec7a3088235b7
Joyce Wambui <jwambui@protonmail.com>
2022-09-01 16:04:21 +0300

    fix firebase error; downgrade version

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { Text } from "react-native";
 import { ThemeProvider } from "styled-components/native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Ionicons } from "@expo/vector-icons";
 import { Navigation } from "./src/infrastructure/navigation";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "@firebase/app";
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
-
 import { theme } from "./src/infrastructure/theme";
-import { RestaurantsScreen } from "./src/features/screens/restaurants.screen";
-import { SafeArea } from "./src/components/utility/safe-area.component";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 
